232cf6dbc28b2d7a81378bec0fe62f7a714a0aa2
pritish <pritishkumar28@gmail.com>
2025-11-12 10:57:42 +0530

    destroy route added/ debug body parsing problem

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const app = express(); 
+const app = express();
 let port = 8080;
 const path = require("path");
-const {v4: uuidv4 } = require("uuid")
+const { v4: uuidv4 } = require("uuid")
 const methodOverride = require("method-override");
 
 //Body parser
@@ -10,73 +10,84 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride('_method'));
 
-app.set("view engine" , "ejs");
-app.set("views" , path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
-        id : uuidv4(),  //ganerate randome id
+        id: uuidv4(),  //ganerate randome id
         username: "pritish",
-        content: "I Love coding",
+        content: "Just completed my Node.js + Express project! 🧠 Learned how RESTful routes work and how to structure clean backend code.",
+        image: "image/post1.jpg",
     },
-     {
-        id : uuidv4(),
+    {
+        id: uuidv4(),
         username: "Ratindra",
-        content: "I am a Student",
+        content: "Designing responsive UI with pure CSS and EJS templates 🎨 Loving how small tweaks make big differences.",
+        image: "image/post2.jpg",
     },
-     {
-        id : uuidv4(),
+    {
+        id: uuidv4(),
         username: "Navo",
-        content: "Hey How are you",
+        content: "Debugged my first major error today 😅 Took 2 hours but the learning was worth it. Never give up on a missing semicolon!",
+        image: "image/post3.jpg",
     },
 ];
 
-app.get("/posts", (req, res)=> {
+app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
 
 //Serve the form
-app.get("/posts/new", (req,res) => {
+app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
 
 //Add new post
-app.post("/posts" , (req , res) => {
-    let { username , content } = req.body;
+app.post("/posts", (req, res) => {
+    let { image, username, content } = req.body;
     let id = uuidv4();
-    posts.push({id, username , content });
+    posts.push({ image, id, username, content });
     res.redirect("/posts");  //redirect to the .get/posts
-    
+
 });
 
 //to get one post
-app.get("/posts/:id" , (req , res) => {
-    let {id} = req.params;
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
     let post = posts.find(p => String(id) === String(p.id));
-    if(!post){
+    if (!post) {
         return res.status(404).send("Page Not Found");
     }
-    res.render("show.ejs" , { post });
+    res.render("show.ejs", { post });
 });
 
 //Update specific post
-app.patch("/posts/:id" , (req , res) => {
-    let {id} = req.params;  //(fixed)  extract id properly
-   const {content} = req.body;
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;  //(fixed)  extract id properly
+    const { content } = req.body;
     let post = posts.find(p => String(id) === String(p.id));
     post.content = content;  //(Fixed)   // extract content from the body (because the form's textarea has name="content")
     res.redirect("/posts");
-}); 
+});
 
 //serve the edit form
-app.get("/posts/:id/edit" , (req, res) => {
-    let {id} = req.params;
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
     let post = posts.find(p => String(id) === String(p.id));
-    res.render("edit.ejs",{post});
+    res.render("edit.ejs", { post });
 
 });
+//Destroy Rout
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter(p => String(id) !== String(p.id));
+    res.redirect("/posts");
+});
+
 
 app.listen(port, () => {
     console.log("server is listening");
